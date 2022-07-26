fcc51deaf2c06753e7426b2e6fd0b0e8a2b06feb
Muna Shrestha <newkanchhu@gmail.com>
2022-07-26 15:00:05 +0545

    init with basic scss setup

---
+++ include/navigation.php
@@ -0,0 +1,17 @@
+<nav class="main-menu">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6 col-md-4">
+                Logo
+            </div>
+            <div class="col-6 col-md-8">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Company</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'include/header.php'; ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Inithive</h1>
+            </div>
+        </div>
+    </div>
+
+<?php include 'include/footer.php'; ?>
